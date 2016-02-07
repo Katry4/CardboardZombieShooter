2422b418d23c8056a1d2b784f061531f5f6b4941
Kratoo5 <kratos666666@gmail.com>
2016-02-07 11:56:29 +0200

    no message

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Bullet : MonoBehaviour {
+
+	public Rigidbody _rigidbody;
+	// Use this for initialization
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	internal void Shoot()
+	{
+		_rigidbody.AddForce(Vector3.forward);
+	}
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+
+	[SerializeField] private Transform _spawnPlace;
+	[SerializeField] private Bullet _bullet;
+
+	public void Shoot()
+	{
+		Bullet bullet = Instantiate(_bullet);
+		bullet.transform.position = _spawnPlace.position;
+		bullet.Shoot();
+	}
+
+	void Update()
+	{
+		//CardboardHead head = Cardboard.Controller.Head;
+		//RaycastHit hit;
+		//bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
+		//SetGazedAt(isLookedAt);
+		if (Cardboard.SDK.Triggered)
+		{
+			Shoot();
+		}
+	}
+}
