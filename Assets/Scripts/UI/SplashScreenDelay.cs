b9ce8c50f23a92b62208d19ba2e346c0d10c9d7e
zippyl <zippy@ex.ua>
2016-05-29 16:47:11 +0300

    splash screen

---
+++ Assets/Scripts/UI/SplashScreenDelay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenDelay : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		StartCoroutine (FadeIn ());
+		yield return new WaitForSeconds (2f);
+		StartCoroutine (FadeOut ());
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (1);
+	}
+
+	IEnumerator FadeIn()
+	{
+		LeanTween.alpha (gameObject, 1f, 1.5f);
+		yield return null;
+	}
+
+	IEnumerator FadeOut()
+	{
+		LeanTween.alpha (gameObject, 0f, 1.5f);
+		yield return null;
+	}
+
+}
